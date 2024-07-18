43b5aea9e647c5af3ec32c4042d4239fab3151ec
minseokPark <qkralstjr.dev@gmail.com>
2024-07-18 23:43:39 +0900

    init: front실습

---
+++ src/components/NumCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+const props = defineProps({
+    num: Number,
+})
+</script>
+
+<template>
+    <div>
+        <!-- {{ num }} -->
+        숫자: <input type="text" v-model="props.num">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,50 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import axios  from 'axios'
+import NumCard from '@/components/NumCard.vue'
 
+const { VITE_REQUEST_URL } = import.meta.env;
+
+const loginForm = ref({
+    email: "",
+    password: "",
+})
+
+const list = ref([1,2,3,4,5])
+
+const login = () => {
+    axios.post(`${VITE_REQUEST_URL}/auth/login`,{
+        email: loginForm.value.email,
+        password: loginForm.value.password,
+    })
+    .then(res => {
+        console.log("로그인 성공")
+        // 페이지 이동
+        alert(1)
+
+    })
+    .catch(err => {
+        alert("로그인 실패");
+    })
+}
+// onMounted(() => {
+//     document.getElementById("")
+// })
 </script>
 
 <template>
     <main>
         <h1>로그인 페이지</h1>
+
+        <form @submit.prevent="login">
+            이메일 <input type="text" v-model="loginForm.email">
+            비밀번호 <input type="text" v-model="loginForm.password">
+            <button type="submit">로그인</button>
+        </form>
+
+        <!-- <div v-for="e in list">
+            <NumCard :num="e"/>
+        </div> -->
     </main>
 </template>
 
